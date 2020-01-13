24a84ddba01ee910d3820a573b36fe273c5575ca
Florian Bouchez <florian.bouchez@sgi-groupe.fr>
2020-01-13 15:36:23 +0100

    update REST

--- REST_Server/api/User/views.py
+++ REST_Server/api/User/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import ArticleSerializer
+from .models import Article
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by('libelle')
+    serializer_class = ArticleSerializer
