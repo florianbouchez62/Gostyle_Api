86e725eee858877d95d228363204db17d163ee85
Florian Bouchez <florian.bouchez@sgi-groupe.fr>
2020-01-14 09:07:14 +0100

    update settings

--- REST_Server/api/api/settings.py
+++ REST_Server/api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9+pucyergttotynph)6=zssayp%wy&5026mfe+l+_ud9+8sh0d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.200']
 
 
 # Application definition
