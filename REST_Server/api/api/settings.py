f8ceffeb99f668377077ea9c3f7de1e037284f21
Florian Bouchez <florian.bouchez@sgi-groupe.fr>
2020-01-14 08:17:42 +0100

    update

--- REST_Server/api/api/settings.py
+++ REST_Server/api/api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'msrpmobile',
+        'USER': 'florianbouchez',
+        'PASSWORD': '29061999',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
