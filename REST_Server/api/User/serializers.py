1e86fd3cbee8c66feda043e1bd9a45a2157c7c79
lucaslemaire <lucas.lemaire.sin@gmail.com>
2020-01-15 09:01:08 +0100

    [ADD] column id autoincrement

--- REST_Server/api/User/serializers.py
+++ REST_Server/api/User/serializers.py
@@ -6,9 +6,9 @@ from .models import Article, Promotion
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Article
-        fields = ('libelle', 'prix')
+        fields = ('id', 'libelle', 'prix')
 
 class PromotionSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Promotion
-        fields = ('article', 'pourcentage', 'qrCode')
+        fields = ('id', 'article', 'pourcentage', 'qrCode')
