6b8e35245a1e5e8259a79f68b51615d80d9b26b2
lucaslemaire <lucas.lemaire.sin@gmail.com>
2020-03-09 22:24:13 +0100

    [UPD] dates null=True

--- REST_Server/api/User/models.py
+++ REST_Server/api/User/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Promotion(models.Model):
     libelle = models.CharField("Libelle", max_length=255, unique=True)
-    start_date = models.DateTimeField('Date début')
-    end_date = models.DateTimeField('Date fin')
+    start_date = models.DateTimeField('Date début', null=True)
+    end_date = models.DateTimeField('Date fin', null=True)
     pourcentage = models.FloatField("Pourcentage", default=1.0)
     description = models.TextField("Description", max_length=1200)
     image = models.ImageField('Image', blank=True, null=True)
