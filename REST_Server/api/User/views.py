09f53cdb074c704eb0685485398eab9913726218
Florian Bouchez <florian.bouchez@sgi-groupe.fr>
2020-03-25 16:37:30 +0100

    remove POST promotions

--- REST_Server/api/User/views.py
+++ REST_Server/api/User/views.py
@@ -1,9 +1,8 @@
-
-from django.shortcuts import render
 from .serializers import PromotionSerializers
 from .models import Promotion
 from rest_framework import viewsets
 
 class PromotionViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'head']
     queryset = Promotion.objects.filter(active=True).order_by('id')
     serializer_class = PromotionSerializers
