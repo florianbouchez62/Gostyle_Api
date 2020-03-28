d8c00820b49fd6cd3d46204e2aca3473782a7307
Lucas Lemaire <lucas.lemaire.sin@gmail.com>
2020-03-28 14:02:51 +0100

    Update settings.py

--- REST_Server/api/api/settings.py
+++ REST_Server/api/api/settings.py
@@ -23,10 +23,10 @@ PROJECT_DIR = dirname(__file__)
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '9+pucyergttotynph)6=zssayp%wy&5026mfe+l+_ud9+8sh0d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 ADMIN_SITE_HEADER = "Gostyle administration"
 
