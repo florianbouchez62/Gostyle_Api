47244798b026a30bb4e90c99e7024de4227068b6
Lucas Lemaire <lucas.lemaire.sin@gmail.com>
2020-03-28 13:56:57 +0100

    update test unitaires

--- REST_Server/api/api/settings.py
+++ REST_Server/api/api/settings.py
@@ -109,11 +109,11 @@ if 'test' in sys.argv:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT_TEST'),
+            'NAME': 'msrpmobile',
+            'USER': 'root',
+            'PASSWORD': 'Froubert100!',
+            'HOST': '127.0.0.1',
+            'PORT': '32768',
         }
     }
 else:
