9f819ad6ffa8498b6d9daf38ac1a5ced8774c02f
lucaslemaire <lucas.lemaire.sin@gmail.com>
2020-07-12 16:17:22 +0200

    upd top 3 promotions

--- REST_Server/api/promotion/views.py
+++ REST_Server/api/promotion/views.py
@@ -7,3 +7,8 @@ class PromotionViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'head']
     queryset = Promotion.objects.filter(active=True).order_by('id')
     serializer_class = PromotionSerializers
+
+class TopPromotionViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'head']
+    queryset = Promotion.objects.filter(active=True).order_by('percentage')
+    serializer_class = PromotionSerializers
